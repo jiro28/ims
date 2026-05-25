3b4cb0540dec1f60120f652410bded498376f8f0
krazey <admin@krazey.de>
2026-05-25 18:20:52 +0200

    singtel_register_pani_test
    Signed-off-by: krazey <admin@krazey.de>

--- app/src/main/java/me/phh/sip/SipRegisterRequestBuilder.kt
+++ app/src/main/java/me/phh/sip/SipRegisterRequestBuilder.kt
@@ -21,6 +21,18 @@ object SipRegisterRequestBuilder {
         val secClientLine = securityClientOverride ?: defaultSecClientLine
 
         // P-Access-Network-Info: 3GPP-E-UTRAN-FDD;utran-cell-id-3gpp=216302ee2003a107
+        // TEMP SingTel REGISTER PANI test.
+        // Current clean matrix: original IMPI digest username, challenge realm,
+        // Proxy-Require: sec-agree, and Security-Verify q=0.5.
+        // This intentionally hardcodes one LTE cell id so we can test whether
+        // SingTel protected REGISTER changes from EOF/no-response to a SIP reply.
+        val singtelPaniLine =
+            if (realm == "ims.mnc001.mcc525.3gppnetwork.org") {
+                "P-Access-Network-Info: 3GPP-E-UTRAN-FDD;utran-cell-id-3gpp=5250102C6B611D01"
+            } else {
+                ""
+            }
+
         return SipRequest(
             SipMethod.REGISTER,
             "sip:$realm",
@@ -30,6 +42,7 @@ object SipRegisterRequestBuilder {
                 Expires: 7200
                 Cseq: $registerCounter REGISTER
                 Contact: $contact
+                $singtelPaniLine
                 Supported: path, gruu, sec-agree
                 Allow: INVITE, ACK, CANCEL, BYE, UPDATE, REFER, NOTIFY, MESSAGE, PRACK, OPTIONS
                 Authorization: $akaDigest
